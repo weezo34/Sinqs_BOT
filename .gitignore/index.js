8e79ca5ee9832a4caa52cd7fb29d7d4e80fb3a3f
weezo34 <51184333+weezo34@users.noreply.github.com>
2019-05-30 05:36:39 +0200

    Ajout de la commande Invite

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,40 @@
 const Discord = require("discord.js");
 
+var PREFIX = "!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function () {
     bot.user.setGame("Sinqs_BOT, !help");
     console.log("Le bot a bien ete conecte");
 });
+
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWish(PREFIX)) return; 
+
+    var args = message.content.substring(PREFIX. length).split(" ");
+
+    switch(args(0).toLowerCase()) {
+        case "invite":
+            message.channel.send("", {
+                embed: {
+                    color: 0xFF0000,
+                    author: message.author.name,
+                    title: '',
+                    field: [{
+                        name: " Lien d'invitation discord",
+                        value: "https://discord.gg/zhcbkc",
+                        inline: false
+                    }],
+                    footer: {
+                        footer: "Partager ce lien à tous vos amis !";
+                },
+            }  
+            });    
+            break;
+    }    
+});       
 
 bot.login("NTgxNTE2MDYwODUxODMwODAx.XO9EXg.U7BfZb2bM28HFuDyaJfdXrXNRjY");
