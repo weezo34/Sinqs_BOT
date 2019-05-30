98a773db18af35597c12f162b0a5f0e82bcecda4
weezo34 <51184333+weezo34@users.noreply.github.com>
2019-05-30 05:55:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ var PREFIX = "!";
 
 var bot = new Discord.Client();
 
-bot.on("ready", function () {
+bot.on("ready", function() {
     bot.user.setGame("Sinqs_BOT, !help");
     console.log("Le bot a bien ete conecte");
 });
@@ -12,28 +12,28 @@ bot.on("ready", function () {
 bot.on("message", async function(message) {
     if (message.author.equals(bot.user)) return;
 
-    if(!message.content.startsWish(PREFIX)) return; 
+    if(!message.content.startsWith(PREFIX)) return; 
 
-    var args = message.content.substring(PREFIX. length).split(" ");
+    var args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args(0).toLowerCase()) {
         case "invite":
-            message.channel.send("", {
-                embed: {
-                    color: 0xFF0000,
-                    author: message.author.name,
-                    title: '',
-                    field: [{
-                        name: " Lien d'invitation discord",
-                        value: "https://discord.gg/zhcbkc",
-                        inline: false
-                    }],
-                    footer: {
-                        footer: "Partager ce lien à tous vos amis !";
+         message.channel.send("", {
+             embed: {
+                color: 0xFF0000,
+                 author: message.author.name,
+                 title: '',
+                 fields: [{
+                     name: "Lien d'invitation discord",
+                     value: "https://discord.gg/zhcbkc",
+                     inline: false
+                }],
+                footer: {
+                    footer: "Partager ce lien à tous vos amis !",
                 },
             }  
         });    
-        break;
+         break;
     }    
 });       
 
